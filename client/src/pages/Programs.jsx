0b92f37896e72fd488ba88c2f480276e72583c1c
DevFaith <waitherafaith@outlook.com>
2024-05-25 11:53:41 +0300

    * feat(Programs.jsx): add Programs component with sections for Creative Expression, Tech Training, LifeSkill Development, Inclusive Activities, and Community Engagement

---
+++ client/src/pages/Programs.jsx
@@ -0,0 +1,37 @@
+const Programs = () => {
+    return (
+      <div className='bg-grays-100 min-h-screen w-full'>
+        <div className='max-w-7xl mx-auto relative'>
+          <img
+            src='./images/image.webp'
+            alt='People joining hands'
+            className='w-full h-[560px] object-cover'
+          />
+          <div className='absolute top-0 left-0 h-full w-full bg-black/60'></div>
+        </div>
+        <div className='mt-20 max-w-7xl mx-auto px-3'>
+          <h2 className='text-3xl md:text-4xl text-reds'>Creative Expression</h2>
+          <p className='mt-10 text-xl md:max-w-[60%]'>Through art therapy, storytelling, music, and dance workshops, we create a fun and supportive environment for children to explore their emotions and creativity. These activities provide a safe space for children to express themselves in unique and meaningful ways, build self-confidence, and develop a love for the arts. Our creative expression programs contribute to emotional and psychological well-being by encouraging self-discovery and helping children process their experiences in a constructive and therapeutic manner.</p>
+          <h2 className='text-3xl mt-32 md:text-4xl text-reds'>Tech Training</h2>
+          <p className='mt-10 text-xl md:max-w-[60%]'>Our free, beginner-friendly courses in coding, digital art, and internet safety provide children with essential skills to confidently navigate the digital world. By introducing children to technology in a safe and structured manner, we prepare them for future educational opportunities and careers. Our tech training emphasizes responsible and secure online behavior, ensuring that children understand the importance of internet safety while exploring and utilizing digital tools and resources.
+
+</p>
+          <h2 className='text-3xl mt-32 md:text-4xl text-reds'>LifeSkill Development</h2>
+          <p className='mt-10 text-xl md:max-w-[60%]'>Practical life skills classes, including cooking, gardening, and basic first aid, teach children independence and self-reliance. These hands-on activities equip children with valuable everyday skills that promote healthy living, environmental awareness, and the ability to handle emergencies. By engaging in these practical tasks, children learn the importance of self-sufficiency, teamwork, and responsibility, fostering well-rounded development and preparing them for various life situations.
+
+</p>
+          <h2 className='text-3xl mt-32 md:text-4xl text-reds'>Inclusive Activities</h2>
+          <p className='mt-10 text-xl md:max-w-[60%]'>We ensure all children, regardless of their background or access to technology, have the opportunity to learn and grow through our inclusive programs. By providing equal access to resources and support, we create a diverse and welcoming environment where every child feels valued and empowered to reach their full potential. Our inclusive approach fosters empathy, respect, and understanding among children from different backgrounds, promoting a harmonious and supportive community.
+
+</p>
+          <h2 className='text-3xl mt-32 md:text-4xl text-reds'>Community Engagement</h2>
+          <p className='mt-10 text-xl md:max-w-[60%]'>Regular community events, outdoor play, and nature activities foster a sense of belonging and encourage children to connect with their peers and environment. These activities help build strong social bonds, enhance teamwork, and instill a sense of responsibility and appreciation for nature. By participating in community engagement initiatives, children learn the value of collaboration, civic duty, and environmental stewardship, promoting a cohesive and engaged community that supports each other.
+
+</p>
+        
+        </div>
+      </div>
+    );
+}
+
+export default Programs
