61f5c7bc43994d29966f66dafe76472de469a9d7
DevFaith <waitherafaith@outlook.com>
2024-05-25 11:52:48 +0300

    * chore(Footer.jsx): update footer text to include author's name

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     <div className='pt-56 bg-grays-100'>
       <div className='max-w-7xl mx-auto'>
         <hr className='w-full bg-grays-300 h-[1px] border-none' />
-        <div className='py-6'>
-          <p>Copyright &copy; 2024 | All rights reserved. </p>
+        <div className='py-6 '>
+          <p>Copyright &copy; 2024 | All rights reserved. brought to you      <span className='text-2xl md:text-2xl text-reds'>by Faith Waithera</span> </p>
         </div>
       </div>
     </div>
