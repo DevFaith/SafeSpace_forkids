d28b1d4aeca2e30ba3946fa1ef5f656d74564aee
DevFaith <waitherafaith@outlook.com>
2024-05-31 11:02:54 +0300

    added vercel.json file

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,16 @@
-
+import Api from "./Generator"
 const Footer = () => {
   return (
-    <div className='pt-56 bg-grays-100'>
+    <div className='pt-56 bg-grays-100 relative'>
       <div className='max-w-7xl mx-auto'>
         <hr className='w-full bg-grays-300 h-[1px] border-none' />
         <div className='py-6 '>
           <p>Copyright &copy; 2024 | All rights reserved. brought to you      <span className='text-2xl md:text-2xl text-reds'>by Faith Waithera</span> </p>
         </div>
+      </div>
+
+      <div className="fixed z-20 ">
+        <Api />
       </div>
     </div>
   )
